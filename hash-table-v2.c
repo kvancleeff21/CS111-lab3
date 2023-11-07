a5359865039eb23c4ece250067ac0bf3cd768a3f
Kaelan van Cleeff <kvancleeff25@g.ucla.edu>
2023-11-07 11:29:16 -0800

    second implementation with v2

--- hash-table-v2.c
+++ hash-table-v2.c
@@ -74,7 +74,6 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
                              uint32_t value)
 {
 	uint32_t index = bernstein_hash(key) % HASH_TABLE_CAPACITY;
-	pthread_mutex_lock(&hash_lock[index]);
 	struct hash_table_entry *hash_table_entry = get_hash_table_entry(hash_table, key);
 	struct list_head *list_head = &hash_table_entry->list_head;
 	struct list_entry *list_entry = get_list_entry(hash_table, key, list_head);
@@ -88,6 +87,7 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
 	list_entry = calloc(1, sizeof(struct list_entry));
 	list_entry->key = key;
 	list_entry->value = value;
+	pthread_mutex_lock(&hash_lock[index]);
 	SLIST_INSERT_HEAD(list_head, list_entry, pointers);
 	pthread_mutex_unlock(&hash_lock[index]);
 }

102178a34ca9a682ad48e37a987cccb7eaecb4d2
Kaelan van Cleeff <kvancleeff25@g.ucla.edu>
2023-11-07 11:25:30 -0800

    first implementation try with v2

--- hash-table-v2.c
+++ hash-table-v2.c
@@ -6,7 +6,7 @@
 #include <sys/queue.h>
 
 #include <pthread.h>
-
+pthread_mutex_t hash_lock[HASH_TABLE_CAPACITY];
 struct list_entry {
 	const char *key;
 	uint32_t value;
@@ -28,6 +28,7 @@ struct hash_table_v2 *hash_table_v2_create()
 	struct hash_table_v2 *hash_table = calloc(1, sizeof(struct hash_table_v2));
 	assert(hash_table != NULL);
 	for (size_t i = 0; i < HASH_TABLE_CAPACITY; ++i) {
+		pthread_mutex_init(&hash_lock[i], NULL);
 		struct hash_table_entry *entry = &hash_table->entries[i];
 		SLIST_INIT(&entry->list_head);
 	}
@@ -72,6 +73,8 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
                              const char *key,
                              uint32_t value)
 {
+	uint32_t index = bernstein_hash(key) % HASH_TABLE_CAPACITY;
+	pthread_mutex_lock(&hash_lock[index]);
 	struct hash_table_entry *hash_table_entry = get_hash_table_entry(hash_table, key);
 	struct list_head *list_head = &hash_table_entry->list_head;
 	struct list_entry *list_entry = get_list_entry(hash_table, key, list_head);
@@ -86,6 +89,7 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
 	list_entry->key = key;
 	list_entry->value = value;
 	SLIST_INSERT_HEAD(list_head, list_entry, pointers);
+	pthread_mutex_unlock(&hash_lock[index]);
 }
 
 uint32_t hash_table_v2_get_value(struct hash_table_v2 *hash_table,
@@ -109,6 +113,7 @@ void hash_table_v2_destroy(struct hash_table_v2 *hash_table)
 			SLIST_REMOVE_HEAD(list_head, pointers);
 			free(list_entry);
 		}
+		pthread_mutex_destroy(&hash_lock[i]);
 	}
 	free(hash_table);
 }

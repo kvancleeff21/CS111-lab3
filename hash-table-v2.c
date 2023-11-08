d1527ca5c4f9597c04eb0d6df6adf9037622d494
Kaelan van Cleeff <kvancleeff25@g.ucla.edu>
2023-11-07 18:18:01 -0800

    around key and value

--- hash-table-v2.c
+++ hash-table-v2.c
@@ -82,11 +82,12 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
 		list_entry->value = value;
 		return;
 	}
-	pthread_mutex_lock(&(hash_table_entry->lock));
+
 	list_entry = calloc(1, sizeof(struct list_entry));
-	pthread_mutex_unlock(&(hash_table_entry->lock));
+	pthread_mutex_lock(&(hash_table_entry->lock));
 	list_entry->key = key;
 	list_entry->value = value;
+	pthread_mutex_unlock(&(hash_table_entry->lock));
 	SLIST_INSERT_HEAD(list_head, list_entry, pointers);
 }
 
